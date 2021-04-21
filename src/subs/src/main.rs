b5da051527b20f84a26afb47c12cc131e5b297a9
Kevin Wenström <kevinwenstrom@localhost.localdomain>
2021-04-21 13:12:07 +0200

    start of solution using KMP

--- src/subs/src/main.rs
+++ src/subs/src/main.rs
@@ -25,58 +25,7 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::string::String;
 
-
-
-fn decrypt_sample(b_message: &[u8], b_fragment: &[u8], adjlist: &Vec<i32>) -> bool {
-
-    let mut dict: HashMap<u8, u8> = HashMap::new();
-    let mut uses: HashSet<u8> = HashSet::with_capacity(26);
-
-    for j in 0..b_fragment.len() {
-        if dict.get(&b_fragment[j]) == None {
-            if !uses.insert(b_message[j]) {
-                return false;
-            }
-            dict.insert(b_fragment[j], b_message[j]);
-            if adjlist[b_fragment[j]as usize -97] != -1{
-                if !(b_message[j] == b_message[j + adjlist[b_fragment[j]as usize -97]as usize]){
-                    return false
-                } 
-            }
-        } else if *dict.get(&b_fragment[j]).unwrap() != b_message[j] {
-            return false;
-        }
-    }
-    true
-}
-
-fn build_adjacency_list(fragment: &[u8]) -> Vec<i32>{
-    let mut list: Vec<i32> = Vec::new();
-    for num in 97..122 {
-        //find first occurence of letter
-        let mut first = -1;
-        for i in 0..fragment.len()-1{
-            if fragment[i] == num{
-                first = i as i32;
-                break;
-            }
-        }
-        if first > 0 {
-            for i in first as usize..fragment.len()-1{
-                if num == fragment[i]{
-                    list.push((i as i32) - first);
-                    break;
-                }
-            }
-            list.push(-1);
-            continue;
-        }
-        list.push(-1)
-    }
-    return list;
-}
-
-fn main() {
+fn main(){
     // get standard input stream
     let input = io::stdin();
 
@@ -89,21 +38,135 @@ fn main() {
     let mut fragment = String::new();
     input.read_line(&mut fragment).expect("IO Error");
     fragment.pop();
+    let txt = message.as_bytes();
+    let pat = fragment.as_bytes();
+    let m = pat.len();
+    let n = txt.len();
+    let mut occurences = 0;
+  
+    // create lps[] that will hold the longest prefix suffix 
+    // values for pattern
+    let mut lps: Vec<usize> = Vec::new();
     
-    let mut occurance = 0;
-    let mut index = 0;
-    let mut latest = 0;
-    let b_fragment = fragment.as_bytes();
-    let adjlist = build_adjacency_list(b_fragment);
-    while index + fragment.len() <= message.len() {
-        if decrypt_sample(&message.as_bytes()[index..(index+fragment.len())], b_fragment, &adjlist) {
-            occurance += 1;
-            latest = index;
+  
+    // Preprocess the pattern (calculate lps[] array)
+    
+    let mut len = 0; // length of the previous longest prefix suffix
+
+    lps[0] = 0; // lps[0] is always 0
+    
+    // the loop calculates lps[i] for i = 1 to M-1
+    let mut i = 1;
+    while i < m{
+        if pat[i]== pat[len]{
+            len += 1;
+            lps[i] = len;
+            i += 1;
+        } else{
+            // This is tricky. Consider the example.
+            // AAACAAAA and i = 7. The idea is similar 
+            // to search step.
+            if len != 0{
+                len = lps[len-1];
+            }   // Also, note that we do not increment i here
+            else{
+                lps[i] = 0;
+                i += 1;
+            }
         }
-        index += 1;
     }
-    if occurance == 1 {
-        println!("{}", message[latest..(latest + fragment.len())].to_string());
+        let mut dict: HashMap<u8, u8> = HashMap::new();
+        let mut uses: HashSet<u8> = HashSet::with_capacity(26);
+
+
+    let mut j: usize = 0; // index for pat[]
+    let mut i: usize = 0; // index for txt[]
+    while i < n{
+        if pat[j] == txt[i]{
+            i += 1;
+            j += 1;
+        }
+        if j == m{
+            occurences += 1;
+            j = lps[j-1];
+        }
+        // mismatch after j matches
+        else {if i < n && pat[j] != txt[i]{
+            // Do not match lps[0..lps[j-1]] characters,
+            // they will match anyway
+            if j != 0{j = lps[j-1]}
+            else{i += 1}
+            }
+        }
     }
-    else {println!("{}", occurance);}
+    if occurences == 1{
+
+    } else {println!("{}", occurences)}
 }
+
+//
+//
+//
+//fn decrypt_sample(b_message: &[u8], b_fragment: &[u8], adjlist: &Vec<i32>) -> bool {
+//
+//    let mut dict: HashMap<u8, u8> = HashMap::new();
+//    let mut uses: HashSet<u8> = HashSet::with_capacity(26);
+//
+//    for j in 0..b_fragment.len() {
+//        if dict.get(&b_fragment[j]) == None {
+//            if !uses.insert(b_message[j]) {
+//                return false;
+//            }
+//            dict.insert(b_fragment[j], b_message[j]);
+//            if adjlist[b_fragment[j]as usize -97] != -1{
+//                if !(b_message[j] == b_message[j + adjlist[b_fragment[j]as usize -97]as usize]){
+//                    return false
+//                } 
+//            }
+//        } else if *dict.get(&b_fragment[j]).unwrap() != b_message[j] {
+//            return false;
+//        }
+//    }
+//    true
+//}
+//
+//fn build_adjacency_list(fragment: &[u8]) -> Vec<i32>{
+//    let mut list: Vec<i32> = Vec::new();
+//    for i in 0..fragment.len(){
+//        
+//
+//    }
+//    return list;
+//}
+//
+//fn main() {
+//    // get standard input stream
+//    let input = io::stdin();
+//
+//    // read message line and remove new-line
+//    let mut message = String::new();
+//    input.read_line(&mut message).expect("IO Error");
+//    message.pop();
+//    
+//    // read fragment line and remove new-line
+//    let mut fragment = String::new();
+//    input.read_line(&mut fragment).expect("IO Error");
+//    fragment.pop();
+//    
+//    let mut occurance = 0;
+//    let mut index = 0;
+//    let mut latest = 0;
+//    let b_fragment = fragment.as_bytes();
+//    let adjlist = build_adjacency_list(b_fragment);
+//    while index + fragment.len() <= message.len() {
+//        if decrypt_sample(&message.as_bytes()[index..(index+fragment.len())], b_fragment, &adjlist) {
+//            occurance += 1;
+//            latest = index;
+//        }
+//        index += 1;
+//    }
+//    if occurance == 1 {
+//        println!("{}", message[latest..(latest + fragment.len())].to_string());
+//    }
+//    else {println!("{}", occurance);}
+//}
