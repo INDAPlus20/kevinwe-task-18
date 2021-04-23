5a3ffb20820d8c9262ece0d13d0679f8bdea586e
Kevin Wenström <kevinwenstrom@localhost.localdomain>
2021-04-23 15:06:55 +0200

    added Eskils solution for subs

--- src/subs/src/main.rs
+++ src/subs/src/main.rs
@@ -1,187 +1,136 @@
-/***
- * Template to a Kattis solution.
- * See: https://open.kattis.com/help/rust
- * Author: Inte Viola Söderlund <violaso@kth.se>
+/**
+ * Solution for Kattis problem ´Chasing Subs´
+ * 
+ * See: https://open.kattis.com/problems/chasingsubs
+ * Author: Eskil Queseth <eskilq>
  */
 
- /*One things that is slowing down your chasingsubs solution is that you're copying a lot;
-
-    Line 8: message[index..(index + fragment.len())].to_string()
-    Line 15: message[latest..(latest + fragment.len())].to_string()
-    Line 17: occurance.to_string()
-    Since both decrypt_message and decrypt_sample are referentially transparent, you can avoid the issue of lifetime parameters by housing all code in main.
-
-Secondly, since all basic alphabet characters are ASCII, they only take up one byte each in a rust string. Therefore, you are able to preallocate memory by String::with_capacity(250_000). Further, to allow for indexation (instead of more cloning fragpiece.clone().nth(i).unwrap()), work with fragment and message as bytes;
-
-    let b_message = message.as_bytes(); and
-    let b_fragment = fragment.as_bytes().
-    String::as_bytes doesn't copy or clone, but returns a slice of type &[u8].
-
-Good luck!
-*/
-
-use std::io;
 use std::collections::HashMap;
-use std::collections::HashSet;
-use std::string::String;
+use std::collections::hash_map::Entry;
+use std::io::{BufRead, stdin};
+use std::num::Wrapping;
 
-fn main(){
-    // get standard input stream
-    let input = io::stdin();
-
-    // read message line and remove new-line
-    let mut message = String::new();
-    input.read_line(&mut message).expect("IO Error");
-    message.pop();
+fn main() {
+    let stdin = stdin();
+    let mut lines = stdin
+        .lock()
+        .lines()
+        .map(|_line| _line.unwrap());
     
-    // read fragment line and remove new-line
-    let mut fragment = String::new();
-    input.read_line(&mut fragment).expect("IO Error");
-    fragment.pop();
-    let txt = message.as_bytes();
-    let pat = fragment.as_bytes();
-    let m = pat.len();
-    let n = txt.len();
-    let mut occurences = 0;
-    let mut last_occurence = 0;
-  
-    // create lps[] that will hold the longest prefix suffix 
-    // values for pattern
-    let mut lps: [usize; 250000] = [0; 250000];
+    let text = lines
+        .next().unwrap()
+        .chars()
+        .map(|_c| _c as u8)
+        .collect::<Vec<_>>();
+    let pattern = lines
+        .next().unwrap()
+        .chars()
+        .map(|_c| _c as u8)
+        .collect::<Vec<_>>();
     
-  
-    // Preprocess the pattern (calculate lps[] array)
+    if pattern.len() > text.len() {
+        println!("0");
+        return;
+    }
     
-    let mut len = 0; // length of the previous longest prefix suffix
-
-    lps[0] = 0; // lps[0] is always 0
+    let mut buckets: Vec<(usize, Wrapping<u64>)> = Vec::new();
+    let mut dict = HashMap::new();
     
-    // the loop calculates lps[i] for i = 1 to M-1
-    let mut i = 1;
-    while i < m{
-        if pat[i]== pat[len]{
-            len += 1;
-            lps[i] = len;
-            i += 1;
-        } else{
-            // This is tricky. Consider the example.
-            // AAACAAAA and i = 7. The idea is similar 
-            // to search step.
-            if len != 0{
-                len = lps[len-1];
-            }   // Also, note that we do not increment i here
-            else{
-                lps[i] = 0;
-                i += 1;
+    for (i, c) in pattern.iter().enumerate() {
+        
+        let num = Wrapping(P.overflowing_pow((pattern.len() - i - 1) as u32).0);
+        
+        match dict.entry(*c) {
+            Entry::Vacant(_entry) => {
+                _entry.insert(buckets.len());
+                buckets.push((i, num));
+            }
+            Entry::Occupied(_entry) => {
+                buckets[*_entry.get()].1 += num;
             }
         }
     }
-
-
-    let mut j: usize = 0; // index for pat[]
-    let mut i: usize = 0; // index for txt[]
-    while i < n{
-        let mut dict: HashMap<u8, u8> = HashMap::new();
-        let mut uses: HashSet<u8> = HashSet::with_capacity(26);
-        for k in 0..pat.len() {
-            if dict.get(&pat[k]) == None {
-                if !uses.insert(pat[k]) {
-                    if j > 0{
-                        j = lps[j-1]
-                    } else {i+=1;}
-                    dict.insert(pat[k], txt[i+k-j]);
-                } else if *dict.get(&pat[k]).unwrap() != txt[i+k-j] {
-                    if j > 0{
-                        j = lps[j-1]
-                    } else {i+=1;}
-                }
+    
+    let mut matches = Vec::new();
+    
+    let iter = HashIterator::new(&text, pattern.len());
+    
+    'outer: for (rolling_hash, text_chunk) in iter.zip(text.windows(pattern.len())) {
+        let mut uses = [false; 256];
+        let mut sum = Wrapping(0);
+        for bucket in &buckets {
+            let c = text_chunk[bucket.0] as u64;
+            if uses[c as usize] {
+                continue 'outer;
+            } else {
+                uses[c as usize] = true;
+                sum += Wrapping(c) * bucket.1;
             }
         }
-        if pat[j] == txt[i]{
-            i += 1;
-            j += 1;
-        }
-        if j == m{
-            occurences += 1;
-            j = lps[j-1];
-            last_occurence = i - m;
-        }
-        // mismatch after j matches
-        else {if i < n && pat[j] != txt[i]{
-            // Do not match lps[0..lps[j-1]] characters,
-            // they will match anyway
-            if j != 0{j = lps[j-1]}
-            else{i += 1}
-            }
+        
+        if sum == rolling_hash {
+            matches.push(text_chunk);
         }
     }
-    if occurences == 1{
-        println!("{}", message[last_occurence..(last_occurence + m)].to_string())
-    } else {println!("{}", occurences)}
+    
+    if matches.len() == 1 {
+        println!("{}", matches[0].iter().map(|e| (*e) as char).collect::<String>());
+    } else {
+        println!("{}", matches.len());
+    }
 }
 
-//
-//
-//
-//fn decrypt_sample(b_message: &[u8], b_fragment: &[u8], adjlist: &Vec<i32>) -> bool {
-//
-//    let mut dict: HashMap<u8, u8> = HashMap::new();
-//    let mut uses: HashSet<u8> = HashSet::with_capacity(26);
-//
-//    for j in 0..b_fragment.len() {
-//        if dict.get(&b_fragment[j]) == None {
-//            if !uses.insert(b_message[j]) {
-//                return false;
-//            }
-//            dict.insert(b_fragment[j], b_message[j]);
-//            if adjlist[b_fragment[j]as usize -97] != -1{
-//                if !(b_message[j] == b_message[j + adjlist[b_fragment[j]as usize -97]as usize]){
-//                    return false
-//                } 
-//            }
-//        } else if *dict.get(&b_fragment[j]).unwrap() != b_message[j] {
-//            return false;
-//        }
-//    }
-//    true
-//}
-//
-//fn build_adjacency_list(fragment: &[u8]) -> Vec<i32>{
-//    let mut list: Vec<i32> = Vec::new();
-//    for i in 0..fragment.len(){
-//        
-//
-//    }
-//    return list;
-//}
-//
-//fn main() {
-//    // get standard input stream
-//    let input = io::stdin();
-//
-//    // read message line and remove new-line
-//    let mut message = String::new();
-//    input.read_line(&mut message).expect("IO Error");
-//    message.pop();
-//    
-//    // read fragment line and remove new-line
-//    let mut fragment = String::new();
-//    input.read_line(&mut fragment).expect("IO Error");
-//    fragment.pop();
-//    
-//    let mut occurance = 0;
-//    let mut index = 0;
-//    let mut latest = 0;
-//    let b_fragment = fragment.as_bytes();
-//    let adjlist = build_adjacency_list(b_fragment);
-//    while index + fragment.len() <= message.len() {
-//        if decrypt_sample(&message.as_bytes()[index..(index+fragment.len())], b_fragment, &adjlist) {
-//            occurance += 1;
-//            latest = index;
-//        }
-//        index += 1;
-//    }
-//    if occurance == 1 {
-//        println!("{}", message[latest..(latest + fragment.len())].to_string());
-//    }
-//    else {println!("{}", occurance);}
+const P: u64 = 69;
+
+struct HashIterator<'a> {
+    text: &'a [u8],
+    rolling_hash: Wrapping<u64>,
+    window_size: usize,
+    position: usize,
+}
+
+impl<'a> HashIterator<'a> {
+    fn new(text: &'a [u8], pattern_len: usize) -> Self {
+        Self {
+            text,
+            rolling_hash: hash(&text[0..pattern_len]),
+            window_size: pattern_len,
+            position: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for HashIterator<'a> {
+    type Item = Wrapping<u64>;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        let out;
+        
+        if self.position <= self.text.len() - self.window_size {
+            out = Some(self.rolling_hash);
+            
+            if self.position != self.text.len() - self.window_size {
+                self.rolling_hash = remove_first(self.rolling_hash, &self.text[self.position..self.position + self.window_size]);
+                self.rolling_hash = self.rolling_hash * Wrapping(P) + Wrapping(self.text[self.position + self.window_size] as u64);
+            }
+            
+            self.position += 1;
+        } else {
+            out = None;
+        }
+        
+        out
+    }
+}
+
+fn hash(to_hash: &[u8]) -> Wrapping<u64> {
+    to_hash.iter()
+        .map(|e| Wrapping(*e as u64))
+        .enumerate()
+        .map(|e| Wrapping(P.overflowing_pow((to_hash.len() - e.0 - 1) as u32).0) * e.1)
+        .fold(Wrapping(0), |acc, e| acc + e)
+}
+
+fn remove_first(old_hash: Wrapping<u64>, hashed: &[u8]) -> Wrapping<u64> {
+    old_hash - Wrapping(hashed[0] as u64) * Wrapping(P.overflowing_pow((hashed.len() - 1) as u32).0)
+}
